3f8ca0be1e063305ba8a8cb8e7eda818a5b8f8ad
pedrops164 <A95826@alunos.uminho.pt>
2025-01-18 17:04:05 +0100

    added cors and csrf protection

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -7,11 +7,15 @@ from flask_login import LoginManager
 from backend.config import Config
 from .error_handlers import register_error_handlers
 from .limiter import limiter
+from flask_cors import CORS
+from flask_wtf.csrf import CSRFProtect
 
 migrate = Migrate()
 login_manager = LoginManager()
 login_manager.login_view = 'bp_authentication.login'
-
+# need to define this better later
+cors = CORS(origins=["http://localhost:8080"], supports_credentials=True)
+csrf = CSRFProtect()
 
 def create_app(config_class=Config):
     app = Flask(__name__)
@@ -22,7 +26,9 @@ def create_app(config_class=Config):
     migrate.init_app(app, db)
     login_manager.init_app(app)
     limiter.init_app(app)
-    
+    cors.init_app(app)
+    #csrf.init_app(app) -> need to do some things with postman. Will active this later
+
     # Import and register Blueprints
     register_blueprints(app)
     
