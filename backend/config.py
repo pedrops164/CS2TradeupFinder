525535898a18cf8cc45446622c57e0ca950d720f
Pedro Sousa <pg54721@alunos.uminho.pt>
2025-02-28 15:53:09 +0000

    added postgres driver dependency so that backend can connect to cloud sql

--- backend/config.py
+++ backend/config.py
@@ -43,7 +43,7 @@ class ProdConfig(BaseConfig):
     DB_HOST = os.environ.get('DB_HOST')
     DB_PORT = os.environ.get('DB_PORT')
     DB_NAME = os.environ.get('DB_NAME')
-    # same as SQLALCHEMY_DATABASE_URI=f"{DB_DRIVERNAME}://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    #SQLALCHEMY_DATABASE_URI=f"{DB_DRIVERNAME}://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
     SQLALCHEMY_DATABASE_URI=sqlalchemy.engine.url.URL.create(
         drivername=DB_DRIVERNAME,
         username=DB_USER,
