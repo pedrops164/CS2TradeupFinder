eaf7dff50fc1caa4e086885ceac9fd14e8e7ebac
Pedro Sousa <pg54721@alunos.uminho.pt>
2025-03-10 15:39:32 +0000

    removed some old commands and sql population scripts

--- backend/app/commands/__init__.py
+++ backend/app/commands/__init__.py
@@ -10,32 +10,6 @@ import os
 from src.scripts.update_prices import update_skin_prices_white_market
 
 def register_commands(app):
-    @app.cli.command("populate-postgres-db")
-    def populate_postgres_db():
-        """Populate the database with initial data."""
-        from sqlalchemy import text
-        with app.app_context():
-            with db.engine.connect() as connection:
-                sql_file_path = os.path.join(os.path.dirname(__file__), 'postgres_dump.sql')
-                with open(sql_file_path, 'r', encoding="utf-8") as file:
-                    query = text(file.read())
-                    connection.execute(query)
-        print("Postgres Database populated!")
-        
-    @app.cli.command("populate-sqlite-db")
-    def populate_sqlite_db():
-        """Populate the SQLite database with initial data."""
-        from sqlalchemy import text
-        with app.app_context():
-            with db.engine.connect() as connection:
-                sql_file_path = os.path.join(os.path.dirname(__file__), 'sqlite_dump.sql')  # Make sure to point to the converted file
-                with open(sql_file_path, 'r', encoding='utf-16') as file:
-                    sql_statements = file.read()
-                    for statement in sql_statements.split(';'):
-                        if statement.strip():
-                            connection.execute(text(statement))
-            print("SQLite Database populated!")
-
 
     @app.cli.command("run-solver")
     def run_solver():
